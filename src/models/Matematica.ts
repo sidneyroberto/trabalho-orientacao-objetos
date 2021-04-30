c468c4ad194b946b996fb6cc37b637628f21c2ee
Sidney Sousa <sidney.sousa@ifms.edu.br>
2021-04-30 11:57:04 -0400

    commit inicial

---
+++ src/models/Matematica.ts
@@ -0,0 +1,28 @@
+export class Matematica {
+
+    somatorio(conjunto: number[]): number {
+        // Implemente corretamente!
+        return 0
+    }
+
+    produto(conjunto: number[]): number {
+        // Implemente corretamente!
+        return 0
+    }
+
+    deltaBhaskara(a: number, b: number, c: number): number {
+        // Implemente corretamente!
+        return 0
+    }
+
+    fatorial(n: number): number {
+        // Implemente corretamente!
+        return 0
+    }
+
+
+    ehPrimo(n: number): boolean {
+        // Implemente corretamente!
+        return false
+    }
+}
